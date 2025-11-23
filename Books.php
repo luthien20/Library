e2cd4afd854131cd198ee6bfc25c8115e3d99fc6
Your Name <anngellamariebarrrios@gmail.com>
2025-11-23 17:50:10 +0800

    bokk

--- Books.php
+++ Books.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     include('db.php');
 
@@ -22,29 +21,4 @@
             echo "All fields are required.";
         }
     }
-=======
-<?php
-    include('db.php');
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if(isset($_POST['title']) && isset($_POST['author']) && isset($_POST['year'])){
-            $title = $_POST['title'];
-            $author = $_POST['author'];
-            $year = $_POST['year'];
-
-            $stmt = $conn->prepare("INSERT INTO books (title, author, year) VALUES (?, ?, ?)");
-            $stmt->bind_param("ssi", $title, $author, $year);
-
-            if($stmt->execute()){
-                echo "New book added successfully.";
-            } else {
-                echo "Error: " . $stmt->error;
-            }
-
-            $stmt->close();
-        } else {
-            echo "All fields are required.";
-        }
-    }
->>>>>>> f618bed497b3adfbce81626dbf242f15d699dcf6
 ?>
